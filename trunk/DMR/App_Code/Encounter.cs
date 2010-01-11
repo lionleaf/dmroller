4f581616ea19bc135ffdc257f31cd7291f6d168b
gwethir <gwethir@23d95166-f9df-11de-ba51-6b4196a2b81e>
2010-01-11 22:39:20 +0000

    Fungerende input nå, med kontroll om at mobnavn er unike!

--- trunk/DMR/App_Code/Encounter.cs
+++ trunk/DMR/App_Code/Encounter.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 
 public class Encounter
 {
     #region Private Members
-    List<Monster> _monsters;
+    List<Monster> _monsters = new List<Monster>();
+	int _nameIncrementer = 1; //To handle multiple names, the names has to be unique, we add a number if they are equal.
     #endregion
 
     #region Properties
@@ -22,7 +24,31 @@ public class Encounter
     #region Public Methods
     public void AddMonster(Monster mob)
     {
+		if (ContainsName(mob.Name))
+		{
+			mob.Name = mob.Name + _nameIncrementer;
+			_nameIncrementer++;
+		}
         _monsters.Add(mob);
     }
+	public string GenerateHTML()
+	{
+		StringBuilder sb = new StringBuilder();
+		foreach (Monster mob in _monsters)
+		{
+			sb.Append(mob.GenerateHTML());
+		}
+		return sb.ToString();
+	}
+
+	public bool ContainsName(string name)
+	{
+		foreach(Monster m in _monsters)
+		{
+			if (m.Name.ToLower() == name.ToLower())
+				return true;
+		}
+		return false;
+	}
     #endregion
 }
--- trunk/DMR/DMR.aspx.cs
+++ trunk/DMR/DMR.aspx.cs
@@ -13,19 +13,47 @@ using System.Xml.Linq;
 
 public partial class index : System.Web.UI.Page
 {
+	public Encounter MainEncounter;
     protected void Page_Load(object sender, EventArgs e)
     {
+		if (Session["MainEncounter"] != null)
+		{
+			MainEncounter = (Encounter)Session["MainEncounter"];
+		}
+		else
+		{
+			MainEncounter = new Encounter();
+		}
 
+		mobAddButton.Attributes.Add("onclick", "javascript:toggle('divtest', 'divmain')");
+
+		MainLabel.Text = MainEncounter.GenerateHTML();
     }
-    protected void btn_submit_Click(object sender, EventArgs e)
+    protected void AddMob(object sender, EventArgs e)
     {
-        //what, did you think I bothered with writing code? ahuarhuarhauhrauhuhuuhuhuhuh.
-        //Hit submit, get the textboxes' values
+		//This be ugly! Need to make it more flexible for an unknown amount of abilites! This is just for testing.
+		Ability[] abilities = {new Ability(mobAb1Name.Text, new DiceSet("d20+"+mobAb1Hit.Text),new DiceSet(mobAb1Damage.Text)),
+								new Ability(mobAb2Name.Text, new DiceSet("d20+"+mobAb2Hit.Text),new DiceSet(mobAb2Damage.Text)),
+								new Ability(mobAb3Name.Text, new DiceSet("d20+"+mobAb3Hit.Text),new DiceSet(mobAb3Damage.Text))
+							  };
+		try
+		{
+			int hp = Convert.ToInt32(mobHP.Text);
+			Monster mob = new Monster(mobName.Text, hp, abilities);
 
-        //Run javascript for toggling visibility if input checks out ok
-        //btn_submit.Attributes.Add("onclick", "javascript:toggle('divtest', 'divmain')");
+			MainEncounter.AddMonster(mob);
+
+			Session["MainEncounter"] = MainEncounter;
+		}
+		catch
+		{
+			mobHP.Style.Add(HtmlTextWriterStyle.BackgroundColor, "red");
+		}
+		
+
         
-
+        //Run javascript for toggling visibility if input checks out ok
+        //btn_submit.Attributes.Add("onclick", "javascript:toggle('divtest', 'divmain')
         
     }
     protected void ddl_mobType_SelectedIndexChanged(object sender, EventArgs e)
