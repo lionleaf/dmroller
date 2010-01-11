8af4b5efacff93f57f6dfc529af7576e59c56f87
Oddweb42 <Oddweb42@23d95166-f9df-11de-ba51-6b4196a2b81e>
2010-01-11 19:28:01 +0000

    Oh, did you want some comments with that?

---
+++ DMR/dat_ass.js
@@ -0,0 +1,13 @@
+﻿function ToggleVisibility(one, two) //Toggles visibility between two divs
+    {
+        if (document.getElementById(one).style.display == "none")
+        {
+            document.getElementById(one).style.display = "block";
+            document.getElementById(two).style.display = "none";
+        }
+        else
+        {
+            document.getElementById(one).style.display = "none";
+            document.getElementById(two).style.display = "block";
+        }
+    }
